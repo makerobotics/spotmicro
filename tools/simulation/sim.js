d14d43e81aec8db1c46e91240a8fc2a58857c4ba
makerobotics <yann.letzner@gmail.com>
2021-10-20 21:00:05 +0200

    Initial simulation

---
+++ tools/simulation/sim.js
@@ -0,0 +1,11 @@
+
+function init() {
+   console.log("Init");
+
+   const c = document.getElementById("myCanvas");
+   var ctx = c.getContext("2d");
+   ctx.moveTo(0, 0);
+   ctx.lineTo(200, 100);
+   ctx.stroke();
+}
+
